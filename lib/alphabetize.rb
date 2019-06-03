66a0d240937198b6d42d3a55efe683bfb952c660
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-06-03 00:41:32 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+
+
 def alphabetize(arr)
   # code here
+  #ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  #ESPERANTO_ALPHABET.split("")
+  arr.sort_by do |phrase| 
+    phrase.delete(" ")
+     
+     
+   
+ end
+
+  
 end
